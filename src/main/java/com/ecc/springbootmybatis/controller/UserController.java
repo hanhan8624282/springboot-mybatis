8ed4750f0745f7d9e0c42acc7355fbfc3b2e14f8
sunchao <3412786980@qq.com>
2022-05-05 16:31:14 +0800

    mabatis-hotfix-4

--- src/main/java/com/ecc/springbootmybatis/controller/UserController.java
+++ src/main/java/com/ecc/springbootmybatis/controller/UserController.java
@@ -33,10 +33,6 @@ public class UserController {
         System.out.println(1234);
         System.out.println(1234);
         System.out.println("这是hot-fix分支");
-        System.out.println("这是hot-fix分支");
-        System.out.println("这是hot-fix分支");
-
-
         return modelAndView;
     }
     //mysql
